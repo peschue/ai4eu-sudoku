f1c97b7f827e3d883c7d5d86e0bc4cb260c06724
Peter Schueller <peter@peterschueller.com>
2020-10-19 10:28:03 +0200

    first version of orchestrator, using separate protobuf file
    also containing old code with separate files, but cannot run due to name
    clashes

---
+++ orchestrator/orchestrator.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+ 
+# orchestrate the sudoku example
+#
+# pseudocode (simple nonparallel case)
+# 
+# while true:
+#   guijob = gui.requestSudokuEvaluation(dummy1)
+#   solverjob = evaluator.evaluateSudokuDesign(guijob)
+#   aspresult = aspsolver.solve(solverjob)
+#   evalresult = evaluator.processEvaluationResult(aspresult)
+#   (dummy2 = )gui.processEvaluationResult(evalresult)
+
+import os
+import logging
+import json
+import time
+import grpc
+import sys
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("importing generated protobuf modules")
+
+sys.path.append('../protobuf/')
+# import sudoku_gui_pb2_grpc
+# import sudoku_gui_pb2
+# import sudoku_design_evaluator_pb2_grpc
+# import sudoku_design_evaluator_pb2
+# import asp_pb2_grpc
+# import asp_pb2
+import orchestrator_pb2_grpc
+import orchestrator_pb2
+
+configfile = "../config.json"
+config = json.load(open(configfile, 'rt'))
+
+def main():
+    gui_channel = grpc.insecure_channel('localhost:'+str(config['gui-grpcport']))
+    # gui_request_stub = sudoku_gui_pb2_grpc.SudokuDesignEvaluationRequestDataBrokerStub(gui_channel)
+    # gui_result_stub = sudoku_gui_pb2_grpc.SudokuDesignEvaluationResultProcessorStub(gui_channel)
+    gui_request_stub = orchestrator_pb2_grpc.SudokuDesignEvaluationRequestDataBrokerStub(gui_channel)
+    gui_result_stub = orchestrator_pb2_grpc.SudokuDesignEvaluationResultProcessorStub(gui_channel)
+
+    evaluator_channel = grpc.insecure_channel('localhost:'+str(config['designevaluator-grpcport']))
+    # evaluator_program_encoder_stub = sudoku_design_evaluator_pb2_grpc.SudokuDesignEvaluationProblemEncoderStub(evaluator_channel)
+    # evaluator_result_decoder_stub = sudoku_design_evaluator_pb2_grpc.SudokuDesignEvaluationResultDecoderStub(evaluator_channel)
+    evaluator_program_encoder_stub = orchestrator_pb2_grpc.SudokuDesignEvaluationProblemEncoderStub(evaluator_channel)
+    evaluator_result_decoder_stub = orchestrator_pb2_grpc.SudokuDesignEvaluationResultDecoderStub(evaluator_channel)
+
+    aspsolver_channel = grpc.insecure_channel('localhost:'+str(config['aspsolver-grpcport']))
+    # aspsolver_stub = asp_pb2_grpc.OneshotSolverStub(aspsolver_channel)
+    aspsolver_stub = orchestrator_pb2_grpc.OneshotSolverStub(aspsolver_channel)
+
+    while True:
+
+        logging.info("calling SudokuDesignEvaluationRequestDataBroker.requestSudokuEvaluation() with empty dummy")
+        #dummy1 = sudoku_gui_pb2.SudokuDesignEvaluationRequest()
+        dummy1 = orchestrator_pb2.SudokuDesignEvaluationRequest()
+        guijob = gui_request_stub.requestSudokuEvaluation(dummy1)
+
+        logging.info("calling SudokuDesignEvaluationProblemEncoder.evaluateSudokuDesign() with payload %s", guijob)
+        solverjob = evaluator_program_encoder_stub.evaluateSudokuDesign(guijob)
+
+        logging.info("calling OneshotSolver.solve() with payload %s", solverjob)
+        aspresult = aspsolver_stub.solve(solverjob)
+
+        logging.info("calling SudokuDesignEvaluationResultDecoder.processEvaluationResult() with payload %s", aspresult)
+        evalresult = evaluator_result_decoder_stub.processEvaluationResult(aspresult)
+
+        logging.info("calling SudokuDesignEvaluationResultProcessor.processEvaluationResult() with payload %s", evalresult)
+        dummy2 = gui_result_stub.processEvaluationResult(evalresult)
+
+main()
