9a2ed847adf393210064755a9d064ed3d4f9270e
Peter Schueller <peter@peterschueller.com>
2020-11-30 18:50:58 +0100

    fixed version specification

--- create-conda-environment.sh
+++ create-conda-environment.sh
@@ -4,4 +4,4 @@
 #
 # The orchestrator just requires grpcio-tools and protobuf.
 
-conda create -n ai4eusudoku -c conda-forge fastapi protobuf grpcio-tools uvicorn python>=3.5 clingo
+conda create -n ai4eusudoku -c conda-forge fastapi protobuf grpcio-tools uvicorn 'python>=3.5' clingo
