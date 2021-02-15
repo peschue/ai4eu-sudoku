23e88495a9a619ada37e83034a26f6377d8a39fb
Peter Schueller <peter@peterschueller.com>
2021-02-15 21:09:34 +0100

    new version for stepping back from enum (not supported by Acumos)

--- versions.sh
+++ versions.sh
@@ -1,7 +1,7 @@
 # define the versions to be used for docker images (building, running, pushing)
 
-export GUI_IMAGE_VERSION=1.2
-export EVALUATOR_IMAGE_VERSION=1.2
+export GUI_IMAGE_VERSION=1.3
+export EVALUATOR_IMAGE_VERSION=1.3
 export ASPSOLVER_IMAGE_VERSION=1.0
 
 # remote repository for pushing images
